39d9250f1d7e11445716f69fff30d8726a626d4d
spire550 <43911342+spire550@users.noreply.github.com>
2024-07-30 17:56:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
   return next();
 });
 const port = process.env.PORT;
-app.use(express.json());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
 app.use("/realEsate", realEstate);
 app.use("/commercial", commercialEstate);
 app.use("/message", message);
