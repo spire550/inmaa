be132a1104e17c0d6cd82e06a400bb8ead4bbf29
Spire550 <ahmedfarid1892@gmail.com>
2024-08-04 01:25:55 +0300

    update commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import connection from "./DB/connection.js";
 import dotenv from "dotenv";
@@ -40,44 +39,3 @@ app.use((err, req, res, next) => {
 });
 await connection();
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-=======
-import express from "express";
-import connection from "./DB/connection.js";
-import dotenv from "dotenv";
-import realEstate from "./src/module/normal/realEstate.router.js";
-import commercialEstate from "./src/module/commercial/commercial.router.js";
-import message from "./src/module/message/message.router.js";
-import bodyParser from "body-parser";
-import cors from "cors";
-dotenv.config();
-const app = express();
-app.use(cors());
-const whitelist = [];
-app.use((req, res, next) => {
-  console.log(req.header("origin"));
-  /*  if (!whitelist.includes(req.header("origin"))) {
-    return next(new Error("Blocked"));
-  } */
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Private-Network", true);
-  return next();
-});
-const port = process.env.PORT;
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.json());
-app.use("/realEsate", realEstate);
-app.use("/commercial", commercialEstate);
-app.use("/message", message);
-
-app.use("*", (req, res) => {
-  return res.json({ message: "Invalid URL" });
-});
-app.use((err, req, res, next) => {
-  return res.json({ message: "Error", err: err.message, stack: err.stack });
-});
-await connection();
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
->>>>>>> a901ac850544f74b0cc222a8eec4134f8da81398
--- src/module/normal/realEstate.controller.js
+++ src/module/normal/realEstate.controller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import estateModel from "../../../DB/models/realEstate.model.js";
 import cloudinaryConnection from "../utils/cloudinary.js";
 export const add = async (req, res, next) => {
@@ -6,7 +5,7 @@ export const add = async (req, res, next) => {
     ownerName,
     ownerID,
     ownerPhone,
-    bankName,//
+    bankName, //
     IBAN,
     email,
     ownerBuildingNum,
@@ -26,9 +25,9 @@ export const add = async (req, res, next) => {
     streetName,
     aprtmentsNum,
     elevatorNum,
-    buildingDay,//
-    buildingMonth,//
-    buildingYear,//
+    buildingDay, //
+    buildingMonth, //
+    buildingYear, //
     ownerAgencyNum,
     ownerAgencyDate,
     ownerAgentName,
@@ -153,92 +152,92 @@ export const add = async (req, res, next) => {
   }
 
   const test = await estateModel.create({
-    ownerName,//
-    ownerID,//
-    ownerPhone,//
-    bankName,//
-    IBAN,//
-    email,//
-    ownerBuildingNum,//
-    postalCode,//
+    ownerName, //
+    ownerID, //
+    ownerPhone, //
+    bankName, //
+    IBAN, //
+    email, //
+    ownerBuildingNum, //
+    postalCode, //
     sakNumber,
-    sakDay,//
-    sakMonth,//
-    sakYear,//
-    floorNum,//
-    mawaqfNum,//
-    buildingName,//
-    dayBD,//
-    monthBD,//
-    yearBD,//
-    addCode,//
-    district,//
-    streetName,//
-    aprtmentsNum,//
-    elevatorNum,//
-    buildingDay,//
-    buildingMonth,//
-    buildingYear,//
-    ownerAgencyNum,//
-    ownerAgencyDate,//
-    ownerAgentName,//
-    ownerAgencyPhone,//
-    ownerAgencyDay,//
-    ownerAgencyMonth,//
-    ownerAgencyYear,//
-    ownerAgencyEmail,//
-    clientName,//
-    clientIDNumber,//
-    clientPhone,//
-    clientEmail,//
-    clientDayBD,//
-    clientMonthBD,//
-    clientYearBD,//
-    clientDayBDHJ,//
-    clientMonthBDHJ,//
-    clientYearBDHJ,//
-    clientAgencyNum,//
-    clientAgencyDate,//
-    clientAgentName,//
-    clientAgencyPhone,//
-    clientAgencyDay,//
-    clientAgencyMonth,//
-    clientAgencyYear,//
-    clientAgencyEmail,//
-    unitNum,//
-    unitFloor,//
-    unitBedRooms,//
-    unitSeats,//
-    unitHalls,//
-    unitMaidRooms,//
-    unitStoreNum,//
-    unitKitchenNum,//
-    unitBathroomNum,//
-    unitYardNum,//
-    unitTypeQ,//
-    unitTypeOther,//
-    unitFornitureQ,//
-    unitKitchenDrawerQ,//
-    unitACQ,//
-    unitACNormalNum,//
-    unitACCnetralNum,//
-    unitSplitNum,//
-    unitWindowNum,//
-    unitElecNum,//
-    unitElecRead,//
-    unitWaterNum,//
-    unitWaterRead,//
-    unitGasNum,//
+    sakDay, //
+    sakMonth, //
+    sakYear, //
+    floorNum, //
+    mawaqfNum, //
+    buildingName, //
+    dayBD, //
+    monthBD, //
+    yearBD, //
+    addCode, //
+    district, //
+    streetName, //
+    aprtmentsNum, //
+    elevatorNum, //
+    buildingDay, //
+    buildingMonth, //
+    buildingYear, //
+    ownerAgencyNum, //
+    ownerAgencyDate, //
+    ownerAgentName, //
+    ownerAgencyPhone, //
+    ownerAgencyDay, //
+    ownerAgencyMonth, //
+    ownerAgencyYear, //
+    ownerAgencyEmail, //
+    clientName, //
+    clientIDNumber, //
+    clientPhone, //
+    clientEmail, //
+    clientDayBD, //
+    clientMonthBD, //
+    clientYearBD, //
+    clientDayBDHJ, //
+    clientMonthBDHJ, //
+    clientYearBDHJ, //
+    clientAgencyNum, //
+    clientAgencyDate, //
+    clientAgentName, //
+    clientAgencyPhone, //
+    clientAgencyDay, //
+    clientAgencyMonth, //
+    clientAgencyYear, //
+    clientAgencyEmail, //
+    unitNum, //
+    unitFloor, //
+    unitBedRooms, //
+    unitSeats, //
+    unitHalls, //
+    unitMaidRooms, //
+    unitStoreNum, //
+    unitKitchenNum, //
+    unitBathroomNum, //
+    unitYardNum, //
+    unitTypeQ, //
+    unitTypeOther, //
+    unitFornitureQ, //
+    unitKitchenDrawerQ, //
+    unitACQ, //
+    unitACNormalNum, //
+    unitACCnetralNum, //
+    unitSplitNum, //
+    unitWindowNum, //
+    unitElecNum, //
+    unitElecRead, //
+    unitWaterNum, //
+    unitWaterRead, //
+    unitGasNum, //
     unitGasRead,
-    unitPrice,//
-    unitInsurance,//
-    unitDay,//
-    unitMonth,//
-    unitYear,//
-    unitPayment,//
-    unitContract,//
-    unitAutoRenewal,//
-    unitNotes,//
+    unitPrice, //
+    unitInsurance, //
+    unitDay, //
+    unitMonth, //
+    unitYear, //
+    unitPayment, //
+    unitContract, //
+    unitAutoRenewal, //
+    unitNotes, //
     ownerImage,
     clientImage,
     sakImage,
@@ -261,267 +260,3 @@ export const getAllData = async (req, res, next) => {
   }
   res.json({ success: true, data });
 };
-=======
-import estateModel from "../../../DB/models/realEstate.model.js";
-import cloudinaryConnection from "../utils/cloudinary.js";
-export const add = async (req, res, next) => {
-  const {
-    ownerName,
-    ownerID,
-    ownerPhone,
-    bankName,
-    IBAN,
-    email,
-    ownerBuildingNum,
-    postalCode,
-    sakNumber,
-    sakDay,
-    sakMonth,
-    sakYear,
-    floorNum,
-    mawaqfNum,
-    buildingName,
-    dayBD,
-    monthBD,
-    yearBD,
-    addCode,
-    district,
-    streetName,
-    aprtmentsNum,
-    elevatorNum,
-    buildingDay,
-    buildingMonth,
-    buildingYear,
-    ownerAgencyNum,
-    ownerAgencyDate,
-    ownerAgentName,
-    ownerAgencyPhone,
-    ownerAgencyDay,
-    ownerAgencyMonth,
-    ownerAgencyYear,
-    ownerAgencyEmail,
-    ///////////////////////////
-    clientName,
-    clientIDNumber,
-    clientPhone,
-    clientEmail,
-    clientDayBD,
-    clientMonthBD,
-    clientYearBD,
-    clientDayBDHJ,
-    clientMonthBDHJ,
-    clientYearBDHJ,
-    clientAgencyNum,
-    clientAgencyDate,
-    clientAgentName,
-    clientAgencyPhone,
-    clientAgencyDay,
-    clientAgencyMonth,
-    clientAgencyYear,
-    clientAgencyEmail,
-    ///////////////////////////////////
-    unitNum,
-    unitFloor,
-    unitBedRooms,
-    unitSeats,
-    unitHalls,
-    unitMaidRooms,
-    unitStoreNum,
-    unitKitchenNum,
-    unitBathroomNum,
-    unitYardNum,
-    unitTypeQ,
-    unitTypeOther,
-    unitFornitureQ,
-    unitKitchenDrawerQ,
-    unitACQ,
-    unitACNormalNum,
-    unitACCnetralNum,
-    unitSplitNum,
-    unitWindowNum,
-    unitElecNum,
-    unitElecRead,
-    unitWaterNum,
-    unitWaterRead,
-    unitGasNum,
-    unitGasRead,
-    unitPrice,
-    unitInsurance,
-    unitDay,
-    unitMonth,
-    unitYear,
-    unitPayment,
-    unitContract,
-    unitAutoRenewal,
-    unitNotes,
-  } = req.body;
-  const ownerImage = [];
-  const clientImage = [];
-  const sakImage = [];
-  const familyImages = [];
-  const agencyImage = [];
-  const agentImage = [];
-
-  if (
-    !req.files.owner ||
-    !req.files.client ||
-    !req.files.sak ||
-    !req.files.family ||
-    !req.files.agency ||
-    !req.files.agent
-  ) {
-    return next({ cause: 400, message: "All Images are required" });
-  }
-  for (const file of req.files["owner"]) {
-    const { secure_url, public_id } =
-      await cloudinaryConnection().uploader.upload(file.path, {
-        folder: `images/realEstate`,
-      });
-    ownerImage.push({ secure_url, public_id });
-  }
-  for (const file of req.files["client"]) {
-    const { secure_url, public_id } =
-      await cloudinaryConnection().uploader.upload(file.path, {
-        folder: `images/realEstate`,
-      });
-    clientImage.push({ secure_url, public_id });
-  }
-  for (const file of req.files["sak"]) {
-    const { secure_url, public_id } =
-      await cloudinaryConnection().uploader.upload(file.path, {
-        folder: `images/realEstate`,
-      });
-    sakImage.push({ secure_url, public_id });
-  }
-  for (const file of req.files["family"]) {
-    const { secure_url, public_id } =
-      await cloudinaryConnection().uploader.upload(file.path, {
-        folder: `images/realEstate`,
-      });
-    familyImages.push({ secure_url, public_id });
-  }
-  for (const file of req.files["agency"]) {
-    const { secure_url, public_id } =
-      await cloudinaryConnection().uploader.upload(file.path, {
-        folder: `images/realEstate`,
-      });
-    agencyImage.push({ secure_url, public_id });
-  }
-  for (const file of req.files["agent"]) {
-    const { secure_url, public_id } =
-      await cloudinaryConnection().uploader.upload(file.path, {
-        folder: `images/realEstate`,
-      });
-    agentImage.push({ secure_url, public_id });
-  }
-
-  const test = await estateModel.create({
-    ownerName,
-    ownerID,
-    ownerPhone,
-    bankName,
-    IBAN,
-    email,
-    ownerBuildingNum,
-    postalCode,
-    sakNumber,
-    sakDay,
-    sakMonth,
-    sakYear,
-    floorNum,
-    mawaqfNum,
-    buildingName,
-    dayBD,
-    monthBD,
-    yearBD,
-    addCode,
-    district,
-    streetName,
-    aprtmentsNum,
-    elevatorNum,
-    buildingDay,
-    buildingMonth,
-    buildingYear,
-    ownerAgencyNum,
-    ownerAgencyDate,
-    ownerAgentName,
-    ownerAgencyPhone,
-    ownerAgencyDay,
-    ownerAgencyMonth,
-    ownerAgencyYear,
-    ownerAgencyEmail,
-    clientName,
-    clientIDNumber,
-    clientPhone,
-    clientEmail,
-    clientDayBD,
-    clientMonthBD,
-    clientYearBD,
-    clientDayBDHJ,
-    clientMonthBDHJ,
-    clientYearBDHJ,
-    clientAgencyNum,
-    clientAgencyDate,
-    clientAgentName,
-    clientAgencyPhone,
-    clientAgencyDay,
-    clientAgencyMonth,
-    clientAgencyYear,
-    clientAgencyEmail,
-    unitNum,
-    unitFloor,
-    unitBedRooms,
-    unitSeats,
-    unitHalls,
-    unitMaidRooms,
-    unitStoreNum,
-    unitKitchenNum,
-    unitBathroomNum,
-    unitYardNum,
-    unitTypeQ,
-    unitTypeOther,
-    unitFornitureQ,
-    unitKitchenDrawerQ,
-    unitACQ,
-    unitACNormalNum,
-    unitACCnetralNum,
-    unitSplitNum,
-    unitWindowNum,
-    unitElecNum,
-    unitElecRead,
-    unitWaterNum,
-    unitWaterRead,
-    unitGasNum,
-    unitGasRead,
-    unitPrice,
-    unitInsurance,
-    unitDay,
-    unitMonth,
-    unitYear,
-    unitPayment,
-    unitContract,
-    unitAutoRenewal,
-    unitNotes,
-    ownerImage,
-    clientImage,
-    sakImage,
-    familyImages,
-    agencyImage,
-    agentImage,
-  });
-  return res.json({ success: true, message: "Data Added Successfully" });
-};
-
-export const getAllData = async (req, res, next) => {
-  let { page } = req.query;
-
-  page = page < 1 || isNaN(page) || !page ? 1 : page;
-  const limit = 1;
-  const skip = limit * (page - 1);
-  const data = await estateModel.find().skip(skip).limit(limit);
-  if (!data.length) {
-    return next(new Error("No Data exists"));
-  }
-  res.json({ success: true, data });
-};
->>>>>>> a901ac850544f74b0cc222a8eec4134f8da81398
